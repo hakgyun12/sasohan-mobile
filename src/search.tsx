014308aeac3ab45badb6da45e29995027d9633d7
LeeBumSeok <qpwoeiru6486@gmail.com>
2021-02-19 02:44:12 +0900

    implement gender and age select Activity

--- src/search.tsx
+++ src/search.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Alert, Text, View, TextInput} from 'react-native';
+import styles from './styles';
+import ButtonToggleGroup from 'react-native-button-toggle-group';
 // @ts-ignore
 import Button from 'apsl-react-native-button'
-import styles from './styles';
-import { timingSafeEqual } from 'crypto';
+// @ts-ignore
+import ModalDropdown from 'react-native-modal-dropdown'
 
 type Userinfo = {
   user_id: string,
@@ -17,15 +19,23 @@ type Userinfo = {
 type nicknameListProps = {
 }
 
+type ageListProps = {
+
+}
+
 interface State {
   user: Userinfo;
   nicknameList: nicknameListProps[];
+  ageList: ageListProps[];
 };
 
 interface Props {
   navigation: any
   route: any
 };
+
+
+
 
 export default class User extends React.Component<Props, State> {
   constructor(props: any) {
@@ -42,7 +52,8 @@ export default class User extends React.Component<Props, State> {
       },
       nicknameList: [
         '', '1233', '1234'
-      ]
+      ],
+      ageList: []
     }
   }
 
@@ -51,29 +62,26 @@ export default class User extends React.Component<Props, State> {
   }
 
   checkNickName =  () => {
-      const chkNickname = function(str: string) {
-        var regNm = /^[0-9a-zA-Z가-힣]{2,8}$/; 
-        return regNm.test(str) ? true : false;
-      };
-      
-      if(chkNickname(this.state.user.tmpNickname) === false) {
-          Alert.alert("한글, 영문, 숫자 대소문자 2~8자리만 사용 가능합니다");
+    const chkNickname = function(str: string) {
+      var regNm = /^[0-9a-zA-Z가-힣]{2,8}$/; 
+      return regNm.test(str) ? true : false;
+    };
+    
+    if(chkNickname(this.state.user.tmpNickname) === false) {
+      this.setState({user: {...this.state.user, tmpNickname: ''}})
+        Alert.alert("한글, 영문, 숫자 대소문자 2~8자리만 사용 가능합니다");
+    }
+    else {
+      if(this.state.nicknameList.indexOf(this.state.user.tmpNickname) > 0) {
+        Alert.alert("이미 존재하는 닉네임입니다.");
       }
       else {
-        if(this.state.nicknameList.indexOf(this.state.user.tmpNickname) > 0) {
-          Alert.alert("이미 존재하는 닉네임입니다.");
-        }
-        else {
-          this.setState({user: {...this.state.user, nickname: this.state.user.tmpNickname}})
-          this.state.nicknameList.push(this.state.user.tmpNickname);
-          console.log(this.state.nicknameList);
-        }
+        this.setState({user: {...this.state.user, nickname: this.state.user.tmpNickname}})
+        this.state.nicknameList.push(this.state.user.tmpNickname);
+        console.log(this.state.nicknameList);
       }
+    }
   }
-
-
-
-  
 
   checkNicknameList = (element: string) => {
     if(element === this.state.user.tmpNickname) {
@@ -81,25 +89,46 @@ export default class User extends React.Component<Props, State> {
     }
   }
 
+  inputAge = () => {
+    for(var i = 0; i < 101; i++) this.state.ageList[i] = i;
+  }
+
   render() {
+    this.inputAge();
     const user_ide = this.props.route.params.user_ide;
     return (
       <View style={styles.container}>
       <Text style={styles.headerText}>닉네임 설정</Text>
-      <Text style={styles.headerText}>id: {user_ide}</Text>
-      <View style={styles.bodyContainer}>
+      <Text style={styles.showText}>id: {user_ide}</Text>
+      <Text style={styles.showText}>nickname: {this.state.user.nickname}</Text>
+      <Text style={styles.showText}>성별: {this.state.user.gender}</Text>
+      <Text style={styles.showText}>나이: {this.state.user.age}</Text>
+      <View>
         <TextInput
           style={styles.textInput}
           onChangeText={(text) => {this.setState({user: {...this.state.user, tmpNickname: text}})}}
-          placeholder="아무거나 입력해주세요."
+          placeholder="닉네임을 입력해주세요."
         />
         <Button 
         style={{backgroundColor: 'blue'}} 
         textstyle={{fontSize: 18}}
         onPress = {this.checkNickName}>
         중복확인
-        </Button> 
-        <Text style = {styles.showText}>{this.state.user.nickname}</Text>
+        </Button>
+        <Text>성별</Text>
+        <ButtonToggleGroup
+          highlightBackgroundColor={'blue'}
+          highlightTextColor={'white'}
+          inactiveBackgroundColor={'transparent'}
+          inactiveTextColor={'grey'}
+          values={['남자', '여자']}
+          onSelect={(val: string) => {this.setState({user: {...this.state.user, gender: val}})}}
+        />
+        <Text>나이</Text>
+        <ModalDropdown
+          options = {this.state.ageList}
+          onSelect = {(idx: number) => {this.setState({user: {...this.state.user, age: idx}})}}>
+        </ModalDropdown>
       </View>
     </View>
     );
